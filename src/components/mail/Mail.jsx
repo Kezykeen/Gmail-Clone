5b0550b263dd0c923d6c12d4dcdada0293d463ad
kezykeen <okorokingsley250@gmail.com>
2021-08-02 09:16:14 +0100

    Added the mail component

---
+++ src/components/mail/Mail.jsx
@@ -0,0 +1,87 @@
+import { IconButton } from "@material-ui/core";
+import {
+  ArrowBack,
+  CheckCircle,
+  Delete,
+  Email,
+  Error,
+  ExitToApp,
+  LabelImportant,
+  MoreVert,
+  MoveToInbox,
+  Print,
+  UnfoldMore,
+  WatchLater,
+} from "@material-ui/icons";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { selectMail } from "../../features/mailSlice";
+import "./Mail.css";
+
+const Mail = () => {
+  const history = useHistory();
+  const { to, subject, message, time } = useSelector(selectMail);
+
+  return (
+    <div className="mail">
+      <div className="mail_tools">
+        <div className="mail_toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBack />
+          </IconButton>
+          <IconButton>
+            <MoveToInbox />
+          </IconButton>
+          <IconButton>
+            <Error />
+          </IconButton>
+          <IconButton>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+          <IconButton>
+            <LabelImportant />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+        <div className="mail_toolsRight">
+          <IconButton>
+            <UnfoldMore />
+          </IconButton>
+          <IconButton>
+            <Print />
+          </IconButton>
+          <IconButton>
+            <ExitToApp />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="mail_body">
+        <div className="mail_bodyHeader">
+          <h2>{subject}</h2>
+          <LabelImportant className="mail_important" />
+          <p>{to}</p>
+          <p className="mail_time">{time}</p>
+        </div>
+
+        <div className="mail_message">
+          <p>{message}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Mail;
