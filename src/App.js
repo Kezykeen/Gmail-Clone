7b327959cf1658ca38feea7e64808398ff01634f
kezykeen <okorokingsley250@gmail.com>
2021-08-02 10:12:30 +0100

    Implemented login/logout functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import Header from "./header/Header";
-import Sidebar from "./sidebar/Sidebar";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Mail from "./components/mail/Mail";
+import EmailList from "./components/emailList/EmailList";
+import SendMail from "./components/sendMail/SendMail";
+import { useDispatch, useSelector } from "react-redux";
+import { selectMessageModalIsOpen } from "./features/mailSlice";
+import { login, logout, selectUser } from "./features/userSlice";
+import Login from "./components/login/Login";
+import { auth } from "./firestore";
 
 function App() {
+  const messageModalIsOpen = useSelector(selectMessageModalIsOpen);
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // the user is logged in
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+
+          <div className="app_body">
+            <Sidebar />
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {messageModalIsOpen && <SendMail />}
+        </div>
+      )}
+    </Router>
   );
 }
 
